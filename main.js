40f6ab299968d18420d236a1886e8c7193764116
ChiefLopes <chieflopesekede@gmail.com>
2023-07-07 03:59:06 +0100

    created 16 boxes using the for loop nested in a for loop and DOM createElement

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-const box = document.querySelector(".boxes").classList.add("big")
+// const box = document.querySelector(".boxes").classList.add("big")
+
+const boxContainer = document.getElementById("box");
+const btn = document.getElementById("btn");
+
+function createBoxes() {
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4; j++){
+            const box = document.createElement("div")
+            console.log(box)
+        }
+    }
+}
+
+createBoxes()
