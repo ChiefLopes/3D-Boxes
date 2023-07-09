066b2dc8e6da4281ae491b0e0835a332ca1d3d35
ChiefLopes <chieflopesekede@gmail.com>
2023-07-10 00:26:29 +0100

    created 16 boxes using the for loop nested in a for loop and DOM createElement to attach a div to each box

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-// const box = document.querySelector(".boxes").classList.add("big")
-
-const boxContainer = document.getElementById("box");
+const boxContainer = document.getElementById("boxes");
 const btn = document.getElementById("btn");
 
 function createBoxes() {
-    for (i = 0; i < 4; i++) {
-        for (j = 0; j < 4; j++){
-            const box = document.createElement("div")
-            console.log(box)
+    for (let i = 0; i < 4; i++) {  // This is the first 4 line for the boxes
+        for (let j = 0; j < 4; j++) { // this creates 4 lines inside the first 4 lines(i) created. that is 4x4 = 16 boxes
+            const box = document.createElement("div"); // this now attaches a div to each line of 4s. making it a div of 4s.
+            console.log(box);
+            
         }
     }
 }
