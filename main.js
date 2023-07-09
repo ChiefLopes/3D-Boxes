9ed905ad2b519ede3e5bba4451a8e9df4a0f9a55
ChiefLopes <chieflopesekede@gmail.com>
2023-07-10 00:58:15 +0100

    restyled the entire css

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-const boxContainer = document.getElementById("boxes");
-const btn = document.getElementById("btn");
 
-function createBoxes() {
-    for (let i = 0; i < 4; i++) {  // This is the first 4 line for the boxes
-        for (let j = 0; j < 4; j++) { // this creates 4 lines inside the first 4 lines(i) created. that is 4x4 = 16 boxes
-            const box = document.createElement("div"); // this now attaches a div to each line of 4s. making it a div of 4s.
-            console.log(box);
-            
-        }
-    }
-}
+// const boxContainer = document.getElementById("boxes");
+// const btn = document.getElementById("btn");
 
-createBoxes()
+// function createBoxes() {
+//   for (let i = 0; i < 4; i++) {// This is the first 4 line for the boxes
+//     for (let j = 0; j < 4; j++) { // this creates 4 lines inside the first 4 lines(i) created. that is 4x4 = 16 boxes
+//         const box = document.createElement("div"); // this now attaches a div to each line of 4s. making it a div of 4s.
+//         box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+//         boxContainer.appendChild(box)
+//     }
+//   }
+// }
+
+// createBoxes();
