bb8fcafa207f4d4f7bc72f5a183861f1673b3cd6
ChiefLopes <chieflopesekede@gmail.com>
2023-07-10 02:17:25 +0100

    experimented and arrived at a fitted background size @main.css line 67

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const boxesContainer = document.querySelector('#boxes');
 const btn = document.querySelector("#btn");
 
+btn.addEventListener("click", () => {
+boxesContainer.classList.toggle("big")})
+
 function createBoxes() {
     for (let i = 0; i < 4; i++) {
         for (let x = 0; x < 4; x++) {
             const box = document.createElement("div")
             box.className = "box"
-            console.log(box);
+            box.style.backgroundPosition = `${-x * 110}px ${-i * 110}px`;
+            boxesContainer.appendChild(box)
+            // console.log(box);
         }
     }
 }
